c4daafeaf3efb1772d7352a16204b5283d4d7b47
gofffro <vazheninstepan@gmail.com>
2025-08-31 15:05:30 +0700

    add host file

---
+++ TGApp/Host.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+
+namespace TGApp
+{
+    class Host
+    {
+        private TelegramBotClient _bot;
+
+        public Host(string token)
+        {
+            _bot = new TelegramBotClient(token);
+        }
+    }
+}
